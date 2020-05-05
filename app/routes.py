05eaa2352cf191949d82c0dc3db885634477be76
Beryl01 <berylnegesa@gmail.com>
2020-05-05 13:46:14 +0300

    working on user welcoming email features

--- app/routes.py
+++ app/routes.py
@@ -16,6 +16,7 @@ def index():
     return render_template('index.html')
 
 @app.route("/product")
+@login_required
 def product():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date.desc()).paginate(page=page, per_page=5)
